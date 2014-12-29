4c77f81867115c016c51cea19ee7de4da113ef71
Ravi Suhag <suhag.ravi@gmail.com>
2014-12-29 18:09:18 +0530

    Cleaning up

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,3 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh'
 
-# Same as above, but in long listing format
-alias ll='ls -GFhl'
-
-alias ri='ri -Tf ansi'
-
-alias mysql='mysql -u root'
-alias mysqladmin='mysqladmin -u root'
-
-alias be='bundle exec'
-alias bx='bundle exec'
-
-alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
-
-alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
-alias rake='noglob rake'
-alias bower='noglob bower'
+# alias ls='ls -GFh'
